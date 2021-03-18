25a141954772df2a5be8a5d64d5340b1e87e36d7
Jakob Torsvik <jmtorsvik@gmail.com>
2021-03-18 14:13:57 +0100

    hei

--- MainCtrl.java
+++ MainCtrl.java
@@ -1,9 +1,14 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class MainCtrl extends DBConn {
 	private String userEmail; // Active user
 	private String courseCode; // Active course
 	private String folderID; // Active folder
 	
-	
+	public String userInput(String consoleMessage) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(consoleMessage);
+		return sc.nextLine();
+	}
 }

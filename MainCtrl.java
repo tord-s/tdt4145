d2f99fb5dea5a1ba32c06158bafcf9e5ea4a8881
Jakob Torsvik <jmtorsvik@gmail.com>
2021-03-19 19:31:06 +0100

    Tjoho

--- MainCtrl.java
+++ MainCtrl.java
@@ -16,8 +16,10 @@ public class MainCtrl implements Runnable {
 	// return sc.nextLine();
 	// }
 
-	// Flyttet connect() og disconnect() fra DBConn hit ettersom vi antageligvis
-	// ikke trenger flere connector-klasser
+	// FLYTTET connect() OG disconnect() FRA DBConn HIT ETTERSOM VI ANTAGELIGVIS IKKE TRENGER FLERE CONNECTOR-KLASSER UANSETT
+	/**
+	 * Initializes the conn field as a Connection to a database
+	 */
 	public void connect() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -32,6 +34,9 @@ public class MainCtrl implements Runnable {
 		}
 	}
 
+	/**
+	 * Closes the Connection conn
+	 */
 	public void disconnect() {
 		try {
 			conn.close();
@@ -51,15 +56,23 @@ public class MainCtrl implements Runnable {
 	 * System.out.println("Please try again"); return false; } }
 	 */
 
-	// Gjorde denne ikke-statisk
+	// GJORDE DENNE IKKE-STATISK
+	/**
+	 * Asks a user for input of email and password and logs in the user if these match
+	 * @param sc For taking input from console
+	 * @return True if email and password match
+	 */
 	private boolean logIn(Scanner sc) {
+		// Take user input of email and password
 		System.out.print("Email:");
 		String email = sc.nextLine();
 		System.out.print("Password:");
 		String password = sc.nextLine();
+		// Initialize a user based on input email
 		User user = new User(email);
 		user.initialize(conn);
-		Boolean valid_login = user.checkPassword(password);
+		// Check if password is correct, and if so, log in user
+		boolean valid_login = user.checkPassword(password);
 		if (valid_login) {
 			userEmail = email;
 			System.out.println("Logged in successfully");
@@ -71,20 +84,24 @@ public class MainCtrl implements Runnable {
 		}
 	}
 
-	// Flyttet denne fra Course til her for føler dette gir mer mening etterom den
-	// uansett trenge et MainCtrl objekt
+	// FLYTTET DENNE FRA COURSE TIL HER.
+	// FØLER DETTE GIR MER MENING ETTERSOM METODEN UANSETT TRENGER ET MainCtrl OBJEKT FOR HENTING AV CONN
+	/**
+	 * Finds all courses that the user participates in
+	 * @return A List of course codes
+	 */
 	public List<String> getCoursesForUser() {
-		List<String> result = new LinkedList<String>();
+		List<String> result = new LinkedList<>();
 		try {
 			String query = "SELECT CourseCode FROM UserInCourse WHERE Email=(?)";
 			PreparedStatement st = conn.prepareStatement(query);
-			st.setString(1, getUserEmail());
+			st.setString(1, userEmail);
 			ResultSet rs = st.executeQuery();
 			while (rs.next()) {
 				result.add(rs.getString("CourseCode"));
 			}
 		} catch (Exception e) {
-			System.out.println("db error during initialization of Courses for User " + getUserEmail());
+			System.out.println("db error during initialization of Courses for User " + userEmail);
 		}
 		return result;
 	}
@@ -105,27 +122,32 @@ public class MainCtrl implements Runnable {
 	 * course.initialize(mainCtrl.conn); }
 	 */
 
-	// Gjorde denne metoden ikke-statisk
+	// GJORDE DENNE METODEN IKKE-STATISK
 	private void selectCourseAndFolder(Scanner sc) {
+		// Find and list all accessible courses
 		List<String> courses = getCoursesForUser();
 		System.out.println("\nYou are following these courses");
 		for (String i : courses) {
 			System.out.println(i);
 		}
+		// Ask for user input on the courseCode of the course to view
 		System.out.println("\nPlease write course code of course you want to view");
 		System.out.print("Course code:");
 		courseCode = sc.nextLine();
+		// Initialize a course based on input courseCode
 		Course course = new Course(courseCode);
 		course.initialize(conn);
+		// Find and list the folders within the course
 		List<String> folders = course.getFolders(conn);
 		System.out.println("\nCourse has following folders:");
 		for (String i : folders) {
 			System.out.println(i);
 		}
+		// Ask for user input on the folderID of the folder to view
 		System.out.println("\nPlease write ID of folder you want to view");
 		System.out.print("ID:");
 		folderID = Integer.parseInt(sc.nextLine());
-		course.initialize(conn);
+		course.initialize(conn); // VET IKKE HVA DENNE LINJEN GJØR
 	}
 
 	@Override
@@ -141,16 +163,11 @@ public class MainCtrl implements Runnable {
 		sc.close();
 	}
 
-	public String getUserEmail() {
-		return userEmail;
-	}
-
 	public static void main(String[] args) {
 		MainCtrl mainCtrl = new MainCtrl();
 		mainCtrl.run();
 
-		// Flyttet dette inn i en egen run() metode i MainCtrl for mer ryddighet og
-		// oversikt
+		// FLYTTET DETTE INN I EN EGEN run() METODE I MainCtrl FOR MER RYDDIGHET OG OVERSIKT I KODEN
 		/*
 		 * mainCtrl.connect();
 		 * System.out.println("\nWelcome to our Piazza-ish application"); Scanner sc =

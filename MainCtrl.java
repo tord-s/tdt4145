3545e7f9d25b2d29182b61c4acf46d2d516ffba5
Jakob Torsvik <jmtorsvik@gmail.com>
2021-03-22 14:24:22 +0100

    Nå kan man poste threads, like de og replye til de

--- MainCtrl.java
+++ MainCtrl.java
@@ -20,7 +20,7 @@ public class MainCtrl implements Runnable {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Properties p = new Properties();
 			p.put("user", "root");
-			p.put("password", "LiteKreativtPass0rd");
+			p.put("password", "toor");
 			conn = DriverManager.getConnection(
 					"jdbc:mysql://localhost:3306/prosjekt?allowPublicKeyRetrieval=true&autoReconnect=true&useSSL=false&serverTimezone=UTC",
 					p);
@@ -53,11 +53,11 @@ public class MainCtrl implements Runnable {
 		String email = sc.nextLine();
 		System.out.print("Password: ");
 		String password = sc.nextLine();
-		
+
 		// Initialize a user based on input email
 		User user = new User(email);
 		user.initialize(conn);
-		
+
 		// Check if password is correct, and if so, log in user
 		boolean valid_login = user.checkPassword(password);
 		if (valid_login) {
@@ -79,13 +79,13 @@ public class MainCtrl implements Runnable {
 		User user = new User(userEmail);
 		user.initialize(conn);
 		user.viewCourses(conn);
-		
+
 		// Ask for user input on the courseCode of the course to view
 		System.out.println("\nPlease write course code of the course you want to view");
 		System.out.print("Course code: ");
 		courseCode = sc.nextLine();
 	}
-	
+
 	/**
 	 * Lists all folders in active course and asks for folder-selection
 	 */
@@ -94,13 +94,13 @@ public class MainCtrl implements Runnable {
 		Course course = new Course(courseCode);
 		course.initialize(conn);
 		course.viewFolders(conn);
-		
+
 		// Ask for user input on the folderID of the folder to view
 		System.out.println("\nPlease write ID of the folder you want to view");
 		System.out.print("ID: ");
 		folderID = Integer.parseInt(sc.nextLine());
 	}
-	
+
 	/**
 	 * Lists all threads in active folder and asks for thread-selection
 	 */
@@ -109,89 +109,61 @@ public class MainCtrl implements Runnable {
 		Folder folder = new Folder(folderID, courseCode);
 		folder.initialize(conn);
 		folder.viewThreads(conn);
-		
+
 		// Asks for user input on the threadID of the thread to view
 		System.out.println();
 		System.out.println("\nPlease write ID of the thread you want to view");
 		System.out.print("ID: ");
 		threadID = Integer.parseInt(sc.nextLine());
 	}
-	
+
 	/**
 	 * Posts a new thread to active folder based on user input
 	 */
 	private void threadPosting() {
 		// Take user input on anonymity if allowed
-		boolean anonymous = false;
 		Course course = new Course(courseCode);
 		course.initialize(conn);
-		if (course.allowsAnonymous()) {
-			System.out.println("\nPost anonymously? (y/n)");
-			System.out.print("Your input: ");
-			String anonymousInput = sc.nextLine();
-			while (true) {
-				if (anonymousInput.equals("y") || anonymousInput.equals("yes")) {
-					anonymous = true;
-					break;
-				} else if (anonymousInput.equals("n") || anonymousInput.equals("no")) {
-					break;
-				} else {
-					System.out.println(anonymousInput + " is not a valid input.");
-					System.out.print("Please try again: ");
-					anonymousInput = sc.nextLine();
-				}
-			}
-		}
-		
+		boolean anonymous = yesNoInput("Post anonymously?");
+
 		// Take user input on content
 		System.out.println("\nWrite the content of the new thread here:");
 		String content = sc.nextLine();
-		
+
 		// Take user input on tags
 		List<String> tags = new LinkedList<>();
-		System.out.println("\nDo you want to add a tag? (y/n)");
-		System.out.print("Your input: ");
-		String addTag = sc.nextLine();
-		while (true) {
-			if (addTag.equals("y") || addTag.equals("yes")) {
-				System.out.print("\nTag: ");
-				tags.add(sc.nextLine());
-				System.out.println("\nDo you want to add another tag? (y/n)");
-				System.out.print("Your input: ");
-				addTag = sc.nextLine();
-			} else if (addTag.equals("n") || addTag.equals("no")) {
-				break;
-			} else {
-				System.out.println("\n'" + addTag + "'" + " is not a valid input");
-				System.out.print("Please try again: ");
-				addTag = sc.nextLine();
-			}
+		boolean addTag = yesNoInput("Do you want to add a tag?");
+		while (addTag) {
+			System.out.print("\nTag: ");
+			tags.add(sc.nextLine());
+			addTag = yesNoInput("Do you want to add another tag?");
 		}
-		
+
 		// Find threadID of new thread
 		List<Integer> threadIDs = course.getThreadIDs();
 		int newID = threadIDs.get(threadIDs.size() - 1) + 1;
-		
-		// Create and save new thread with email set to null if thread is posted anonymously
+
+		// Create and save new thread with email set to null if thread is posted
+		// anonymously
 		new Thread(newID, courseCode, content, anonymous ? null : userEmail, folderID, tags).save(conn);
-		
+
 		// Create and save two empty replies
-		int studReplyID = (int) (Integer.MAX_VALUE*Math.random());
-		int instReplyID = (int) (Integer.MAX_VALUE*Math.random());
+		int studReplyID = (int) (Integer.MAX_VALUE * Math.random());
+		int instReplyID = (int) (Integer.MAX_VALUE * Math.random());
 		try {
 			new Reply(studReplyID, null, null, newID, courseCode, "StudentsAnswer").save(conn);
 			new Reply(instReplyID, null, null, newID, courseCode, "InstructorsAnswer").save(conn);
 		} catch (Exception e) {
 			System.out.println("Error during instantiation of Reply");
 		}
-		
+
 		// Confirmation for user
 		Folder folder = new Folder(folderID, courseCode);
 		folder.initialize(conn);
 		String folderName = folder.getName();
 		System.out.println("\nThread posted to folder " + folderName);
 	}
-	
+
 	/**
 	 * Registers if the user likes the post and if they would like to leave a reply
 	 */
@@ -202,59 +174,40 @@ public class MainCtrl implements Runnable {
 			st.setInt(1, threadID);
 			st.setString(2, courseCode);
 			st.setString(3, userEmail);
-			
+
 			// Take user input on if they like the post
-			System.out.println("\nDo you like this post? (y/n)");
-			System.out.print("Your input: ");
-			String likesPost = sc.nextLine();
-			while (true) {
-				if (likesPost.equals("y") || likesPost.equals("yes")) {
-					st.setInt(4, 1);
-					st.setInt(5, 1);
-					break;
-				} else if (likesPost.equals("n") || likesPost.equals("no")) {
-					st.setInt(4, 0);
-					st.setInt(5, 0);
-					break;
-				} else {
-					System.out.println(likesPost + " is not a valid input");
-					System.out.print("Please try again: ");
-					likesPost = sc.nextLine();
-				}
+			if (yesNoInput("Do you like this post?")) {
+				st.setInt(4, 1);
+				st.setInt(5, 1);
+			} else {
+				st.setInt(4, 0);
+				st.setInt(5, 0);
 			}
 			st.executeUpdate();
 		} catch (Exception e) {
 			System.out.println("db error while reading Thread " + threadID + ", " + courseCode);
 		}
-		
+
 		// Take user input on if they would like to reply to thread
-		System.out.println("Would you like to reply to this post? (y/n)");
-		System.out.println("Your input: ");
-		String toReply = sc.nextLine();
-		while (true) {
-			if (toReply.equals("y") || toReply.equals("yes")) {
-				replyToThread();
-				break;
-			} else if (toReply.equals("n") || toReply.equals("no")) {
-				break;
-			} else {
-				System.out.println(toReply + "is not a valid input");
-				System.out.print("Please try again: ");
-				toReply = sc.nextLine();
-			}
+		if (yesNoInput("Would you like to reply to this post?")) {
+			replyToThread();
 		}
 	}
-	
+
+	/**
+	 * Takes user input on reply and creates a reply entity either of type
+	 * "StudentsAnswer" or "InstructorsAnswer" based on the users role in the course
+	 */
 	private void replyToThread() {
 		// Create and initialize user to get role in course
 		User user = new User(userEmail);
 		user.initialize(conn);
 		String role = user.roleInCourse(courseCode, conn);
-		
+
 		// Create and initialize thread
 		Thread thread = new Thread(threadID, courseCode);
 		thread.initialize(conn);
-		
+
 		// Check if the reply corresponding to the users role is already replied to
 		int replyID = 0;
 		if (role.equals("Student")) {
@@ -264,107 +217,156 @@ public class MainCtrl implements Runnable {
 		}
 		Reply reply = new Reply(replyID);
 		reply.initialize(conn);
-		
+
 		// Only continue if thread is not replied to
 		if (reply.getContent() != null) {
 			System.out.println("This thread already has a reply by a(n) " + role);
 			return;
-		} 
-		
+		}
+
 		// Take user input on anonymity if allowed
 		boolean anonymous = false;
 		Course course = new Course(courseCode);
 		course.initialize(conn);
 		if (course.allowsAnonymous()) {
-			System.out.println("\nReply to post anonymously? (y/n)");
-			System.out.print("Your input: ");
-			String anonymousInput = sc.nextLine();
-			while (true) {
-				if (anonymousInput.equals("y") || anonymousInput.equals("yes")) {
-					anonymous = true;
-					break;
-				} else if (anonymousInput.equals("n") || anonymousInput.equals("no")) {
-					break;
-				} else {
-					System.out.println(anonymousInput + " is not a valid input.");
-					System.out.print("Please try again: ");
-					anonymousInput = sc.nextLine();
-				}
-			}
+			anonymous = yesNoInput("Reply to post anonymously?");
 		}
-		
+
 		// Take user input on content
 		System.out.println("\nWrite the content of the reply here:");
 		String content = sc.nextLine();
-		
+
 		// Create and save a new reply
 		try {
-			new Reply(replyID, userEmail, content, threadID, courseCode, role.equals("Student") ? "StudentsAnswer" : "InstructorsAnswer").save(conn);
+			new Reply(replyID, userEmail, content, threadID, courseCode,
+					role.equals("Student") ? "StudentsAnswer" : "InstructorsAnswer").save(conn);
 		} catch (Exception e) {
 			System.out.println("Error during instantiaion of Reply");
 		}
-		
+
 		// Confirmation for user
 		System.out.println("Reply given to Thread " + threadID);
 	}
 
+	/**
+	 * Takes user input on if they would like to view statistics, and if so, prints
+	 * out user statistics like how many posts created etc.
+	 */
 	private void viewStatistics() {
-		System.out.println("\n Would you like to see statistics for how many posts users have created and read? (y/n)");
-		String answer = sc.nextLine();
-		if (answer.equals("y") || answer.equals("yes")) {
-			try {
-				String query = "SELECT A.Email, ThreadsRead, ThreadsCreated" +
-				" FROM (SELECT Email, count(userreads.ThreadID) as ThreadsRead" +
-				" FROM user LEFT OUTER JOIN userreadsthread as userreads USING(Email)" +
-				" group by Email order by ThreadsRead desc) AS A" +
-				" LEFT OUTER JOIN (SELECT Email, count(ThreadID) as ThreadsCreated" +
-				" FROM thread group by thread.Email) AS B" +
-				" ON A.Email=B.Email;";
-				PreparedStatement st = conn.prepareStatement(query);
-				ResultSet rs = st.executeQuery();
-				System.out.println("Statistics:");
-				ResultSetMetaData rsmd = rs.getMetaData();
-				int columnsNumber = rsmd.getColumnCount();
-				while (rs.next()) {
-					for (int i = 1; i <= columnsNumber; i++) {
-						if (i > 1) System.out.print(",  ");
-						String columnValue = rs.getString(i);
-						System.out.print(columnValue + " " + rsmd.getColumnName(i));
+		/*
+		 * System.out.
+		 * println("\n Would you like to see statistics for how many posts users have created and read? (y/n)"
+		 * ); String answer = sc.nextLine(); while (true) { if (answer.equals("y") ||
+		 * answer.equals("yes")) { try { String query =
+		 * "SELECT A.Email, ThreadsRead, ThreadsCreated" +
+		 * " FROM (SELECT Email, count(userreads.ThreadID) as ThreadsRead" +
+		 * " FROM user LEFT OUTER JOIN userreadsthread as userreads USING(Email)" +
+		 * " group by Email order by ThreadsRead desc) AS A" +
+		 * " LEFT OUTER JOIN (SELECT Email, count(ThreadID) as ThreadsCreated" +
+		 * " FROM thread group by thread.Email) AS B" + " ON A.Email=B.Email;";
+		 * PreparedStatement st = conn.prepareStatement(query); ResultSet rs =
+		 * st.executeQuery(); System.out.println("Statistics:"); ResultSetMetaData rsmd
+		 * = rs.getMetaData(); int columnsNumber = rsmd.getColumnCount(); while
+		 * (rs.next()) { for (int i = 1; i <= columnsNumber; i++) { if (i > 1) {
+		 * System.out.print(",  "); // LUREST Å ALLTID BRUKE BRACKETS SELV NÅR MAN IKKE
+		 * TRENGER // ETTERSOM DET GJØR DET MER SIKKERT Å DEBUGGE I ETTERKANT } String
+		 * columnValue = rs.getString(i); System.out.print(columnValue + " " +
+		 * rsmd.getColumnName(i)); } System.out.println("\n"); } } catch (Exception e) {
+		 * System.out.println("Error while retriving statistics " + e); } break; } else
+		 * if (answer.equals("n") || answer.equals("no")) { break; } else {
+		 * System.out.println(answer + " is not a valid input");
+		 * System.out.print("Please try again: "); answer = sc.nextLine(); } }
+		 */
+
+		// HAR FLYTTET SPØRRINGEN TIL run()
+
+		try {
+			String query = "SELECT A.Email, ThreadsRead, ThreadsCreated"
+					+ " FROM (SELECT Email, count(userreads.ThreadID) as ThreadsRead"
+					+ " FROM user LEFT OUTER JOIN userreadsthread as userreads USING(Email)"
+					+ " group by Email order by ThreadsRead desc) AS A"
+					+ " LEFT OUTER JOIN (SELECT Email, count(ThreadID) as ThreadsCreated"
+					+ " FROM thread group by thread.Email) AS B" + " ON A.Email=B.Email;";
+			PreparedStatement st = conn.prepareStatement(query);
+			ResultSet rs = st.executeQuery();
+			System.out.println("Statistics:");
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int columnsNumber = rsmd.getColumnCount();
+			while (rs.next()) {
+				for (int i = 1; i <= columnsNumber; i++) {
+					if (i > 1) {
+						System.out.print(",  "); // LUREST Å ALLTID BRUKE BRACKETS SELV NÅR MAN IKKE TRENGER
+													// ETTERSOM DET GJØR DET MER SIKKERT Å DEBUGGE I ETTERKANT
 					}
-					System.out.println("\n");
+					String columnValue = rs.getString(i);
+					System.out.print(columnValue + " " + rsmd.getColumnName(i));
 				}
-			} catch (Exception e) {
-				System.out.println("Error while retriving statistics " + e);
+				System.out.println("\n");
 			}
-		} else if (answer.equals("n") || answer.equals("no")) {
-			return;
+		} catch (Exception e) {
+			System.out.println("Error while retriving statistics " + e);
 		}
 	}
-	
+
+	private boolean yesNoInput(String question) {
+		boolean result = false;
+		System.out.println("\n" + question + " (y/n)");
+		System.out.print("Your answer: ");
+		String answer = sc.nextLine();
+		while (true) {
+			if (answer.equals("y") || answer.equals("yes")) {
+				result = true;
+				break;
+			} else if (answer.equals("n") || answer.equals("no")) {
+				break;
+			} else {
+				System.out.println(answer + " is not a valid input");
+				System.out.print("Please try again: ");
+				answer = sc.nextLine();
+			}
+		}
+		return result;
+	}
+
 	@Override
 	public void run() {
 		// Connect to database
 		connect();
-		
+
 		// Welcome user and create a Scanner for input
 		System.out.println("\nWelcome to our Piazza-ish application");
 		sc = new Scanner(System.in);
-		
+
 		// Log in
 		boolean successfull_login = logIn();
 		while (!successfull_login) {
 			successfull_login = logIn();
 		}
 
-		viewStatistics();
-		
-		// Selection of course and folder
+		// Option to view statistics
+		if (yesNoInput("Would you like to see statistics for how many posts users have created and read?")) {
+			viewStatistics();
+		}
+
+		// Selection of course
 		courseSelection();
+
+		// BURDE IKKE DET EGENTLIG GJØRES PÅ DENNE MÅTEN? ER DET IKKE KUN INSTRUCTORS
+		// SOM KAN SE STATISTIKK? OG ER DET IKKE I SÅ TILFELLE KUN INNAD I DET COURSET
+		// HVOR DE ER INSTRUCTOR?
+//		// Instructors may view statistics
+//		User user = new User(userEmail);
+//		user.initialize(conn);
+//		if (user.roleInCourse(courseCode, conn).equals("Instructor")) {
+//			viewStatistics();
+//		}
+
+		// Selection of folder
 		folderSelection();
-		
-		
+
 		// Get input from user on if they would like to browse or post threads
-		System.out.println("\nWrite 'browse' if you would like to browse existing threads \nor 'post' if you would like to post a new thread");
+		System.out.println(
+				"\nWrite 'browse' if you would like to browse existing threads \nor 'post' if you would like to post a new thread");
 		System.out.print("Your input: ");
 		String browseOrPost = sc.nextLine();
 		while (true) {
@@ -383,7 +385,7 @@ public class MainCtrl implements Runnable {
 			System.out.println("Please try again:");
 			browseOrPost = sc.nextLine();
 		}
-			
+
 		// Close scanner and disconnect from database
 		sc.close();
 		disconnect();
